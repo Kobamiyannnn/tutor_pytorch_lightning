2c51deb79355a67c93908356811fe32b9205a0d1
Kobamiyannnn <kobaprfm23@gmail.com>
2024-11-28 16:01:43 +0900

    [wip] level_1のコーディング

---
+++ src/level_up/basic_skills/level_1/train_a_model.py
@@ -0,0 +1,41 @@
+# Train A Model (Basic)
+# Audience: Users who need to train a model without coding their own training loops.
+
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import lightning as L
+
+
+# Define the PyTorch nn.Modules
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+
+    def forward(self, x):
+        return self.l1(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+    def forward(self, x):
+        return self.l1(x)
+    
+
+# Define a LightningModule
+class LitAutoEncoder(L.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        
